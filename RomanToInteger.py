93ea8f24736dc401a5e5d7c2bf45ee812cae0d41
rumeysacoskun <73069961+o87702623@users.noreply.github.com>
2022-09-08 11:00:36 +0300

    Update RomanToInteger.py

--- RomanToInteger.py
+++ RomanToInteger.py
@@ -1,24 +1,24 @@
-def solution(string):
+def romanToInt(string):
     dict = {"I": 1, "IV": 4, "V": 5, "IX": 9, "X": 10,
             "XL": 40, "L": 50, "XC": 90, "C": 100,
             "CD": 400, "D": 500, "CM": 900, "M": 1000}
 
-    digits = []
+    values = []
 
     while len(string) != 0:
         if len(string) == 1:
-            digits.append(dict[string])
+            values.append(dict[string])
             string = string[:-1]
 
         elif dict[string[-1]] <= dict[string[-2]]:
-            digits.append(dict[string[-1]])
+            values.append(dict[string[-1]])
             string = string[:-1]
 
         else:
-            digits.append(dict[string[-2:]])
+            values.append(dict[string[-2:]])
             string = string[:-2]
 
     return sum(digits)
 
 
-print(solution("MCMXCIV"))
+print(romanToInt("MCMXCIV"))
